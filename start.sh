b58b2e133aadeaea7a2cdb4bfd1e3712f7ad2a2a
Mac Champion <github@champgm.e4ward.com>
2018-06-19 18:25:28 -0400

    Tweak start.sh to use path where script is located

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t local-emailer .
-docker run -it --rm -p 4200:4200 local-emailer
+docker build -t local-emailer `dirname "$0"`
+docker run --rm -p 4200:4200 local-emailer
